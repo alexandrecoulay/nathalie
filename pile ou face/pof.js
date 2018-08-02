4816ef53109b917c49e60d94da4058ae79e6b945
alexandre coulay <35507235+alexandrecoulay@users.noreply.github.com>
2018-08-02 18:34:22 +0200

    Add files via upload

---
+++ pile ou face/pof.js
@@ -0,0 +1,42 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+const cfg = require('../index.json');
+bot.on('ready', function () {
+    console.log("pof")
+  }); 
+
+
+  bot.on('message', function(message) {
+const prefix = ("!");
+const args = message.content.slice(prefix.length).split(/ +/);
+const command = args.shift().toLowerCase();
+
+if (command === 'flip'){
+     const flip = Math.floor(Math.random() * 2) == 0 ? "face" : "pile"
+    const pile = "pile";
+    const face = "face";
+    let gagnant = message.guild.roles.find("name", "gagant");
+    let perdant = message.guild.roles.find("name", "perdant");
+    if (!args.length) return message.channel.send(`Mettre pile ou face, ${message.author}!`);
+
+    message.channel.send(`Result: **${flip}**!`);
+    if (flip == args){
+        message.channel.send("gagné");
+        message.member.addRole(gagnant).catch(console.error);
+        message.channel.send("tu as le role gagnant");
+        message.member.removeRole(perdant).catch(console.error);
+
+    } else {
+        message.channel.send("perdu");
+        message.channel.send("tu as le role perdant");
+        message.member.addRole(perdant).catch(console.error);
+        message.member.removeRole(gagnant).catch(console.error);
+    }
+    
+  }  
+
+});
+
+
+
+bot.login(cfg.token);
