c6d576c1dbca6a25d1e4d07404916c6611bcc415
alexandre coulay <35507235+alexandrecoulay@users.noreply.github.com>
2018-09-09 14:45:22 +0200

    Update member_count.js

--- message embed/member_count.js
+++ message embed/member_count.js
@@ -7,9 +7,12 @@ bot.on('ready', function () {
   });
 
 bot.on('message', message => {
-
+let roledev = message.guild.roles.find("name", "dev-bot-discord").members.size;
+let roleabo = message.guild.roles.find("name", "abonné").members.size;
 if (message.content === prefix + "member"){
-	message.channel.send(`Nombre de membre :  **${message.guild.memberCount}**`, true);
+	message.channel.send(`Nombre de membre :  **${message.guild.memberCount}**`, true)
+	message.channel.send(`Nombre de membre avec le role dev-bot-discord :  **${roledev}**`, true)
+	message.channel.send(`Nombre d'abonné' :  **${roleabo}**`, true)
 	}
 });
 
