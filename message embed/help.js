d5776f6e190fadc15813c0b2c6826ba9d55cbc4f
alexandre coulay <35507235+alexandrecoulay@users.noreply.github.com>
2018-09-16 12:53:25 +0200

    Add files via upload

---
+++ message embed/help.js
@@ -0,0 +1,20 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+const cfg = require('../index.json');
+const prefix = ("!");
+bot.on('ready', function () {
+    console.log("help")
+  });
+
+function couleur()  {
+    return  "#" + Math.floor(Math.random()*16777215).toString(16);
+}
+
+bot.on('message', function(msg) {
+    if (msg.content.startsWith("!help") ){
+        msg.channel.send("<#464333808741646347>");
+
+    }
+});
+
+bot.login(cfg.token);
